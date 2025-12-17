87b2626eeef30afa8f26fa540f29f4636fb84b32
Denise Deng <siktang1221@gmail.com>
2025-12-17 15:49:38 -0500

    Custom form control

--- src/components/AddMember.tsx
+++ src/components/AddMember.tsx
@@ -12,6 +12,7 @@ const AddMemberForm = ({ onClose }: { onClose: () => void }) => {
     };
     
     const [formData, setFormData] = useState(INITIAL_STATE);
+    const [submitClicked, setSubmitClicked] = useState(false);
 
     const queryClient = useQueryClient();
 
@@ -22,7 +23,7 @@ const AddMemberForm = ({ onClose }: { onClose: () => void }) => {
             queryClient.invalidateQueries({ queryKey: ["teamMembers"] });
 
             setFormData(INITIAL_STATE);
-
+            setSubmitClicked(false);
             onClose();
         },
         onError: () => {
@@ -32,6 +33,18 @@ const AddMemberForm = ({ onClose }: { onClose: () => void }) => {
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
+
+        setSubmitClicked(true);
+
+        if (
+            !formData.email ||
+            !formData.name ||
+            !formData.role ||
+            !formData.bio
+        ) {
+            return;
+        }
+
         mutation.mutate(formData);
     };
 
@@ -53,26 +66,46 @@ const AddMemberForm = ({ onClose }: { onClose: () => void }) => {
     return (
         <div>
             <h3>Add New Member</h3>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} noValidate>
                 <div className="form__input">
                     <label htmlFor="name">Name</label>
                     <input id="name" name="name" required value={formData.name} onChange={handleChange} />
                 </div>
+                {formData.name === "" && submitClicked && 
+                    <div className="error-field">
+                        This field is required!
+                    </div>
+                }
                 
                 <div className="form__input">
                     <label htmlFor="role">Role</label>
-                    <input id="role" name="role" required value={formData.role} onChange={handleChange} />
+                    <input id="role" name="role" required value={formData.role} onChange={handleChange} />    
                 </div>
+                {formData.role === "" && submitClicked && 
+                    <div className="error-field">
+                        This field is required!
+                    </div>
+                }
 
                 <div className="form__input">
                     <label htmlFor="email">Email</label>
                     <input id="email" type="email" name="email" required value={formData.email} onChange={handleChange} />
                 </div>
+                {formData.email === "" && submitClicked && 
+                    <div className="error-field">
+                        This field is required!
+                    </div>
+                }
 
                 <div className="form__input">
                     <label htmlFor="bio">Bio</label>
                     <textarea id="bio" name="bio" required value={formData.bio} onChange={handleChange} />
                 </div>
+                {formData.bio === "" && submitClicked && 
+                    <div className="error-field">
+                        This field is required!
+                    </div>
+                }
 
                 {mutation.isPending ?  (
                     <button className="button button__disabled" disabled>
